313d2bab229ba517314fc064020aeb3b02e76eee
Your Name~Manie2005 <you@example.com>
2024-09-09 08:47:36 +0100

    ADD BANKY.cpp file

---
+++ BANKY.cpp
@@ -0,0 +1,319 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	maKan:
+	system("color 3F");
+	std::cout<<"***************WELCOME TO BANKLIFY*****************"<<endl;
+	std::cout<<"POWERED BY MANIE INC."<<endl;
+	std::cout<<"1) OPEN AN ACCOUNT"<<endl;
+	std::cout<<"2) ENTER EXISTING ACCOUNT"<<endl;
+	std::cout<<"1 OR 2?"<<endl;
+	string a;
+	cin>>a;
+	if(a=="1"){
+		system("CLS");
+		system("color 0C");
+		std::cout<<"ENTER YOUR USERNAME"<<endl;
+		string b;
+		cin>>b;
+		std::cout<<"HELLO "<<b<<" WELCOME TO BANKLIFY.\nWE'RE GLAD TO HAVE YOU ON BOARD."<<endl;
+		std::cout<<"ENTER YOUR AGE"<<endl;
+		int c;
+		cin>>c;
+		if(c>=18){
+			std::cout<<"AGE PERMITTED\n";
+			std::cout<<"ENTER YOUR PASSWORD\n";
+		    system("CLS");
+		    std::cout<<"TO GENERATE PASSWORD FOLLOW STEPS BELOW:"<<endl; 
+			std::cout<<"FIRST LETTER OF YOUR LASTNAME"<<endl;
+			string ab;
+			cin>>ab;
+			std::cout<<"LAST  DIGIT FROM YEAR OF BIRTH"<<endl;
+			int cd;
+			cin>>cd;
+			std::cout<<"FIRST TWO LETTERS OF YOUR MIDDLE NAME"<<endl;
+			string ef;
+			cin>>ef;
+			int _randNum=rand()%10+1;
+			std::cout<<_randNum<<endl;
+			std::cout<<"COMBINE ALL\n"<<endl;
+			std::cout<<ab<<cd<<ef<< _randNum<<" IS YOUR PASSWORD"<<endl;
+		std::cout<<"CONFIRM THE GENERATED PASSWORD"<<endl;	
+		string aby;
+		cin>>aby;
+			std::cout<<"PROCEED TO YOUR LOGIN PAGE\nHOPE YOU'LL HAVE A GREAT TIME WITH US";
+			system("CLS");
+			system("color 3C");
+			std::cout<<"********************WELCOME TO BANKLIFY***************************"<<endl;
+            std::cout<<"   **************GLAD TO HAVE YOU ON BOARD********************"<<endl;
+std::cout<<"ENTER YOUR  FIRST NAME"<<endl;
+string xy;
+cin>>xy;
+
+std::cout<<"INPUT YOUR GENERATED PASSWORD"<<endl;
+string axy;
+cin>>axy;
+if(axy==aby||b==xy){
+	std::cout<<"PASSWORD CONFIRMED"<<endl;
+
+	
+IGREATER:
+const int zero=0;
+	int eee;
+	int acct=eee+zero;
+	int rrr;
+	int rex=eee-rrr;
+	system("CLS");
+	system("color 0C");
+	ACB:
+			std::cout<<"ACCT NUMBER="<<01234567432;
+	std::cout<<"WELCOME "<<b<<" TO YOUR PERSONAL DASHBOARD"<<endl;
+	
+	std::cout<<"ACCOUNT BALANCE ="<<acct<<endl;  
+	std::cout<<"PASSWORD=*********"<<endl;
+	std::cout<<"WOULD YOU LIKE TO DEPOSIT MONEY INTO THE ACCOUNT"<<endl;
+	std::cout<<"YES OR NO"<<endl;
+	string mno;
+	cin>>mno;
+	std::cout<<"HOW WOULD YOU LIKE TO DEPOSIT THE MONEY"<<endl;
+	std::cout<<"1.)THROUGH BANK DEPOSITS"<<endl;
+		std::cout<<"2.)PAYPAL"<<endl;	
+				std::cout<<"3.)CRYPTOCURRENCY"<<endl;	
+		std::cout<<"4.)GIFT CARD"<<endl;	
+		std::cout<<"SELECT FROM 1 THROUGH 4."<<endl;	
+string est;
+if(est=="1"){
+			std::cout<<"INPUT YOUR 10 DIGIT ACCOUNT NUMBER"<<endl;	
+string fdj;
+		std::cout<<"ENTER "<<endl;	
+
+	
+}
+	std::cout<<"TRANSFER TO OTHER BANKS";
+	std::cout<<"YES OR NO";
+	string ghj;
+	cin>>ghj;
+	if(ghj=="yes"||ghj=="YES"){
+		std::cout<<"SELECT FROM THE BANKS BELOW(1-4):";
+		std::cout<<"1)ZENITH BANK";
+		std::cout<<"2)ACCESS BANK";
+		std::cout<<"3)F.C.M.B";
+		std::cout<<"4)G.T.B";	
+	}
+	string malone;
+	cin>>malone;
+	if(malone=="1"){
+			system("CLS");
+	system("color 3A");
+		std::cout<<"ENTER YOUR ZENITH USERNAME ";
+		std::cout<<"ENTER ACCT NUMBER FOR TRANSFER";
+		std::cout<<"INPUT NAME OF RECIPIENT";
+		string recipient;
+		cin>>recipient;
+		std::cout<<"ACCOUNT DETAILS CONFIRMED";
+			system("CLS");
+	system("color 3B");
+		std::cout<<"HOW MUCH DO YOU WANT TO SEND TO "<<recipient;
+		int rrr;
+		 rrr==eee;
+				std::cout<<"SUCCESSFULLY TRANSFERED "<<rrr<<" TO "<<recipient;
+std::cout<<"YOU HAVE "<<rex<<"LEFT IN YOUR ACCOUNT";
+	}
+	if(mno=="YES"||mno=="yes"){
+		std::cout<<"HOW WOULD YOU LIKE TO DEPOSIT THE MONEY"<<endl;
+		std::cout<<"1) BANK TRANSFER"<<endl;	
+			std::cout<<"2) PAYPAL"<<endl;
+			std::cout<<"3) CRYPTO"<<endl;
+		int nyt;
+		cin>>nyt;
+		if(nyt==1){
+			std::cout<<"WHICH OF OUR CORPORATE BANKS WOULD YOU LIKE TO TRANSFER FROM?";
+						std::cout<<"1) ZENITH BANK\n";
+						std::cout<<"2) ACCESS BANK\n";
+						std::cout<<"3) F.C.M.B\n";
+						std::cout<<"4)G.T.B\n";
+						
+						int bnk;
+						cin>>bnk;
+						if(bnk==1){
+							std::cout<<" INPUT YOUR ZENITH BANK USERNAME"<<endl;
+							string znb;
+							cin>>znb;
+													std::cout<<"YOUR ZENITH PASSWORD"<<endl;
+string znp;
+cin>>znp;
+system("CLS");
+system("color 9F");
+std::cout<<"WELCOME TO ZENITH BANK TRANSFER WINDOW";
+std::cout<<"ENSURE THAT ALL TRANSFERS ARE DULY CONFIRMED"<<endl;
+std::cout<<"HOW MUCH WOULD YOU LIKE TO TRANSFER?"<<endl;
+
+cin>>eee;
+std::cout<<"CONFIRM TRANSFER:"<<endl;
+std::cout<<"YES OR NO"<<endl;
+string www;
+cin>>www;
+if(www=="yes"||www=="YES"){
+	
+	std::cout<<"YOU HAVE SUCCESSFULLY TRANSFERED"<< eee<<" TO BANKLIFY"<<endl;
+system("CLS");
+	
+	goto IGREATER;
+	
+}
+						}
+							if(bnk==2){
+							std::cout<<" INPUT YOUR ACCESS BANK USERNAME";
+							string acb;
+							cin>>acb;
+													std::cout<<"YOUR ACCESS PASSWORD";
+string acp;
+cin>>acp;
+system("CLS");
+system("color 8C");
+std::cout<<"WELCOME TO ACCESS BANK TRANSFER WINDOW"<<endl;
+
+std::cout<<"HOW MUCH WOULD YOU LIKE TO TRANSFER?"<<endl;
+
+
+std::cout<<"CONFIRM TRANSFER:"<<endl;
+std::cout<<"YES OR NO"<<endl;
+string www;
+cin>>www;
+if(www=="yes"||www=="YES"){
+	
+	
+system("CLS");
+	
+	goto IGREATER;
+	
+
+						}	if(bnk==3){
+							std::cout<<" INPUT YOUR F.C.M.B BANK USERNAME";
+							string fcb;
+							cin>>fcb;
+													std::cout<<"YOUR ZENITH PASSWORD";
+string fcp;
+cin>>fcp;
+						}
+	if(bnk==1){
+							std::cout<<" INPUT YOUR G.T.B BANK USERNAME";
+							string gtb;
+							cin>>gtb;
+													std::cout<<"YOUR G.T.B PASSWORD";
+string gtp;
+cin>>gtp;
+						}
+			
+			
+			
+			
+			
+			
+			
+			
+		}	
+		
+		
+	}
+	else{
+		std::cout<<"WE'D LOVE YOU TO BANK WITH US "<<b<<"\nWE'LL BE WAITING FOR YOU ";
+			std::cout<<"WOULD YOU LIKE TO RETURN HOME"<<endl;
+				std::cout<<"YES OR NO"<<endl;
+	string hgf;
+	cin>>hgf;
+	if(hgf=="YES"||hgf=="yes")
+
+	
+	{system("CLS");
+		goto maKan;	
+	
+	}
+	else{
+		system("CLS");
+	}
+	}
+	
+	
+	
+	
+	
+}
+else{
+	std::cout<<"PASSWORD DOES NOT MATCH NAME "<<endl;
+	std::cout<<"CONFIRM PASSWORD AGAIN"<<endl;
+	string stu;
+	cin>>stu;
+	std::cout<<"CONFIRM YOUR USERNAME AGAIN";
+	string ghf;
+	cin>>ghf;
+	if(stu==aby||ghf==b){
+		std::cout<<"PASSWORD IS NOW CONFIRMED"<<endl;
+	goto IGREATER;
+	}
+}
+
+
+
+
+std::cout<<"";
+
+
+	}	else{
+			std::cout<<"AGE RESTRICTED";
+			system("CLS");
+			system("color 0F");
+			std::cout<<"SORRY "<<b<<" BUT YOU ARE BELOW OUR MINIMUM AGE,\nWE'D STILL LOVE TO BANK WITH YOU IN THE FUTURE\nMUCH LOVE HERE FROM BANKLIFY\nTHANKS FOR USING OUR SOFTWARE "<<endl;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+}
